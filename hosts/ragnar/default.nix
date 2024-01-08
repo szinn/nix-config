b2889aaf0b8cbcb6db827c3dcb35dd9ed7732d4f
Scotte Zinn <scotte@zinn.ca>
2024-01-08 16:58:36 -0500

    Update to k3s 1.29

--- hosts/ragnar/default.nix
+++ hosts/ragnar/default.nix
@@ -29,7 +29,7 @@
     services = {
       k3s = {
         enable = true;
-        package = pkgs-unstable.k3s_1_28;
+        package = pkgs-unstable.k3s_1_29;
         extraFlags = [
           "--tls-san=nas.zinn.ca"
         ];
