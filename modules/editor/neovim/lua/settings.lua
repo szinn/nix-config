c951739df77b50be0778cf8c1596bf4a69fc028f
Scotte Zinn <scotte@zinn.ca>
2024-02-16 14:57:06 -0500

    Strip nvim config to minimum

--- modules/editor/neovim/lua/settings.lua
+++ modules/editor/neovim/lua/settings.lua
@@ -2,17 +2,16 @@
 -- Settings
 -- ===========================================================================
 
-vim.filetype.add({
-    pattern = {
-        [".*%.tfvars"] = "terraform",
-        [".*%.tf"] = "terraform",
-        [".*%.rasi"] = "rasi",
-    },
-})
+-- vim.filetype.add({
+--     pattern = {
+--         [".*%.tfvars"] = "terraform",
+--         [".*%.tf"] = "terraform",
+--     },
+-- })
 
-vim.api.nvim_create_autocmd("FileType", {
-    pattern = "*.eml",
-    callback = function()
-        vim.o.wrapmargin = 79 -- Wrap text automatically
-    end,
-})
+-- vim.api.nvim_create_autocmd("FileType", {
+--     pattern = "*.eml",
+--     callback = function()
+--         vim.o.wrapmargin = 79 -- Wrap text automatically
+--     end,
+-- })
