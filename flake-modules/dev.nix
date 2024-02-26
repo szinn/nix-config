6120bd45af9060b30d518197ca54c30b19b17f0d
Scotte Zinn <scotte@zinn.ca>
2024-02-26 16:34:34 -0500

    Use flake-parts for modularity (#39)

---
+++ flake-modules/dev.nix
@@ -0,0 +1,17 @@
+{inputs, ...}: {
+  perSystem = {pkgs, ...}: {
+    devShells.default = pkgs.mkShellNoCC {
+      packages = with pkgs; [
+        age
+        git
+        gnupg
+        jq
+        nix
+        sops
+        ssh-to-age
+      ];
+    };
+
+    formatter = pkgs.alejandra;
+  };
+}
