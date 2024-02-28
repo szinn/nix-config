c6f572784e6cb8ed2fb1630fbc6d161af6f4a64f
Scotte Zinn <scotte@zinn.ca>
2024-02-27 20:08:57 -0500

    Docs framework

--- homes/modules/development/default.nix
+++ homes/modules/development/default.nix
@@ -1,7 +1,15 @@
-{...}: {
+{pkgs, ...}: {
   imports = [
     ./go
     ./postgres
     ./rust
   ];
+
+  config = {
+    home.packages = with pkgs; [
+      mdbook
+      mdbook-admonish
+      mdbook-toc
+    ];
+  };
 }
