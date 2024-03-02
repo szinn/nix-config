5d60a791a4ce9aaee0286240513349e0d48f3431
Scotte Zinn <scotte@zinn.ca>
2024-03-02 11:10:01 -0500

    Add stateVersion comment

--- hosts/modules/darwin/default.nix
+++ hosts/modules/darwin/default.nix
@@ -8,5 +8,6 @@
     ./ui-defaults.nix
   ];
 
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = 4;
 }
--- hosts/modules/nixos/default.nix
+++ hosts/modules/nixos/default.nix
@@ -27,5 +27,6 @@
     }
   ];
 
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "23.11";
 }
