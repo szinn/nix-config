86430d36396639104c395d01a5a3d40dc41714dc
Scotte Zinn <scotte@zinn.ca>
2024-03-03 17:52:20 -0500

    Add monitor definition

--- homes/scotte/hera/hyprland/config.nix
+++ homes/scotte/hera/hyprland/config.nix
@@ -1,6 +1,7 @@
 {...}: {
   wayland.windowManager.hyprland.settings = {
     exec-once = "/nix/store/h3zv7932ymvq41z7dh92v02h9pmnb583-dbus-1.14.10/bin/dbus-update-activation-environment --systemd DISPLAY HYPRLAND_INSTANCE_SIGNATURE WAYLAND_DISPLAY XDG_CURRENT_DESKTOP & systemctl --user stop hyprland-session.target & systemctl --user start hyprland-session.target & waybar & wayvnc 0.0.0.0 & alacritty";
+    "monitor" = "HDMI-A-1,1920x1080@50,0x0,1";
 
     env = [
       "XDG_CURRENT_DESKTOP,Hyprland"
