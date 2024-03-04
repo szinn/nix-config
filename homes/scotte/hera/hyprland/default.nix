c788406a2cc4ca416bdfa5f645a06116180aa7c9
Scotte Zinn <scotte@zinn.ca>
2024-03-04 09:09:56 -0500

    More hyprland config

--- homes/scotte/hera/hyprland/default.nix
+++ homes/scotte/hera/hyprland/default.nix
@@ -1,10 +1,24 @@
-{pkgs, ...}: {
-  imports = [
-    ./config
-    ./waybar
-  ];
-
+{
+  inputs,
+  pkgs,
+  lib,
+  ...
+}: let
+  hyprland_waybar = pkgs.waybar.overrideAttrs (oldAttrs: {
+    mesonFlags = oldAttrs.mesonFlags ++ ["-Dexperimental=true"];
+    postPatch = ''
+      sed -i 's/zext_workspace_handle_v1_activate(workspace_handle_);/const std::string command = "hyprctl dispatch workspace " + name_;\n\tsystem(command.c_str());/g' src/modules/wlr/workspace_manager.cpp
+    '';
+  });
+in {
   wayland.windowManager.hyprland.enable = true;
+
+  programs.waybar = {
+    enable = true;
+    package = hyprland_waybar;
+    systemd.enable = true;
+    systemd.target = "hyprland-session.target";
+  };
 
   systemd.user.services.wayVNC = {
     Unit = {
@@ -22,6 +36,11 @@
     };
 
     Install = {WantedBy = ["hyprland-session.target"];};
+  };
+
+  services.dunst = {
+    enable = true;
+    configFile = ./configs/dunstrc;
   };
 
   gtk = {
@@ -52,6 +71,37 @@
       Settings = ''
         gtk-application-prefer-dark-theme=1
       '';
+    };
+  };
+
+  xdg.configFile = {
+    "hypr/hyprland.conf" = {
+      text =
+        lib.mkForce
+        (''
+            exec-once = ${pkgs.dbus}/bin/dbus-update-activation-environment --systemd --all && systemctl --user restart hyprland-session.target
+          ''
+          + builtins.readFile ./configs/hyprland.conf);
+      onChange = ''
+        (  # execute in subshell so that `shopt` won't affect other scripts
+          shopt -s nullglob  # so that nothing is done if /tmp/hypr/ does not exist or is empty
+          for instance in /tmp/hypr/*; do
+            HYPRLAND_INSTANCE_SIGNATURE=''${instance##*/} ${inputs.hyprland.packages.${pkgs.system}.hyprland}/bin/hyprctl reload config-only \
+              || true  # ignore dead instance(s)
+          done
+        )
+      '';
+    };
+
+    "waybar" = {
+      source = ./configs/waybar;
+      recursive = true;
+      onChange = ''systemctl --user restart waybar'';
+    };
+
+    "rofi" = {
+      source = ./configs/rofi;
+      recursive = true;
     };
   };
 }
