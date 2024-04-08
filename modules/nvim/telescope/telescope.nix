1af1615e0e4e648d22ede927f760e87288113ac5
Scotte Zinn <scotte@zinn.ca>
2024-04-08 08:00:08 -0400

    Fix telescope plugin config

--- modules/nvim/telescope/telescope.nix
+++ modules/nvim/telescope/telescope.nix
@@ -14,76 +14,77 @@
         };
         undo = {
           enable = true;
-          mappings = {
+          settings.mappings = {
             i = {
-              "<CR>" = "yank_additions";
-              "<s-cr>" = "yank_deletions";
-              "<c-cr>" = "restore";
+              "<CR>" = "require('telescope-undo.actions').yank_additions";
+              "<s-cr>" = "require('telescope-undo.actions').yank_deletions";
+              "<c-cr>" = "require('telescope-undo.actions').restore";
             };
             n = {
-              "y" = "yank_additions";
-              "Y" = "yank_deletions";
-              "u" = "restore";
+              "y" = "require('telescope-undo.actions').yank_additions";
+              "Y" = "require('telescope-undo.actions').yank_deletions";
+              "u" = "require('telescope-undo.actions').restore";
             };
           };
         };
       };
+      settings = {
+        keymaps = {
+          "<leader>sh" = {
+            action = "help_tags";
+            desc = "[S]earch [H]elp";
+          };
+          "<leader>sk" = {
+            action = "keymaps";
+            desc = "[S]earch [K]eymaps";
+          };
+          "<leader>sf" = {
+            action = "find_files";
+            desc = "[S]earch [F]iles";
+          };
+          "<leader>ss" = {
+            action = "builtin";
+            desc = "[S]earch [S]elect Telescope";
+          };
+          "<leader>sw" = {
+            action = "grep_string";
+            desc = "[S]earch Current [W]ord";
+          };
+          "<leader>sg" = {
+            action = "live_grep";
+            desc = "[S]earch by [G]rep";
+          };
+          "<leader>sd" = {
+            action = "diagnostics";
+            desc = "[S]earch [D]iagnostics";
+          };
+          "<leader>sr" = {
+            action = "resume";
+            desc = "[S]earch [R]esume";
+          };
+          "<leader>s." = {
+            action = "oldfiles";
+            desc = "[S]earch Recent Files (. for repeat)";
+          };
+          "<leader><leader>" = {
+            action = "buffers";
+            desc = "[] Find existing buffers";
+          };
+        };
 
-      keymaps = {
-        "<leader>sh" = {
-          action = "help_tags";
-          desc = "[S]earch [H]elp";
-        };
-        "<leader>sk" = {
-          action = "keymaps";
-          desc = "[S]earch [K]eymaps";
-        };
-        "<leader>sf" = {
-          action = "find_files";
-          desc = "[S]earch [F]iles";
-        };
-        "<leader>ss" = {
-          action = "builtin";
-          desc = "[S]earch [S]elect Telescope";
-        };
-        "<leader>sw" = {
-          action = "grep_string";
-          desc = "[S]earch Current [W]ord";
-        };
-        "<leader>sg" = {
-          action = "live_grep";
-          desc = "[S]earch by [G]rep";
-        };
-        "<leader>sd" = {
-          action = "diagnostics";
-          desc = "[S]earch [D]iagnostics";
-        };
-        "<leader>sr" = {
-          action = "resume";
-          desc = "[S]earch [R]esume";
-        };
-        "<leader>s." = {
-          action = "oldfiles";
-          desc = "[S]earch Recent Files (. for repeat)";
-        };
-        "<leader><leader>" = {
-          action = "buffers";
-          desc = "[] Find existing buffers";
-        };
-      };
+        keymapsSilent = true;
 
-      keymapsSilent = true;
-
-      defaults = {
-        file_ignore_patterns = [
-          "^.git/"
-          "^.mypy_cache/"
-          "^__pycache__/"
-          "^output/"
-          "^data/"
-          "%.ipynb"
-        ];
-        set_env.COLORTERM = "truecolor";
+        defaults = {
+          file_ignore_patterns = [
+            "^.git/"
+            "^.mypy_cache/"
+            "^__pycache__/"
+            "^output/"
+            "^data/"
+            "%.ipynb"
+          ];
+          set_env.COLORTERM = "truecolor";
+        };
       };
     };
   };
