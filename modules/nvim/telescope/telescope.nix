53cdbd1fd10bf2477d590cb5e1bd872206aac94e
Scotte Zinn <scotte@zinn.ca>
2024-04-14 06:29:59 -0400

    fix(nvim): Fix telescope keymap config

--- modules/nvim/telescope/telescope.nix
+++ modules/nvim/telescope/telescope.nix
@@ -28,52 +28,51 @@
           };
         };
       };
-      settings = {
-        keymaps = {
-          "<leader>sh" = {
-            action = "help_tags";
-            desc = "[S]earch [H]elp";
-          };
-          "<leader>sk" = {
-            action = "keymaps";
-            desc = "[S]earch [K]eymaps";
-          };
-          "<leader>sf" = {
-            action = "find_files";
-            desc = "[S]earch [F]iles";
-          };
-          "<leader>ss" = {
-            action = "builtin";
-            desc = "[S]earch [S]elect Telescope";
-          };
-          "<leader>sw" = {
-            action = "grep_string";
-            desc = "[S]earch Current [W]ord";
-          };
-          "<leader>sg" = {
-            action = "live_grep";
-            desc = "[S]earch by [G]rep";
-          };
-          "<leader>sd" = {
-            action = "diagnostics";
-            desc = "[S]earch [D]iagnostics";
-          };
-          "<leader>sr" = {
-            action = "resume";
-            desc = "[S]earch [R]esume";
-          };
-          "<leader>s." = {
-            action = "oldfiles";
-            desc = "[S]earch Recent Files (. for repeat)";
-          };
-          "<leader><leader>" = {
-            action = "buffers";
-            desc = "[] Find existing buffers";
-          };
+      keymaps = {
+        "<leader>sh" = {
+          action = "help_tags";
+          options.desc = "[S]earch [H]elp";
         };
+        "<leader>sk" = {
+          action = "keymaps";
+          options.desc = "[S]earch [K]eymaps";
+        };
+        "<leader>sf" = {
+          action = "find_files";
+          options.desc = "[S]earch [F]iles";
+        };
+        "<leader>ss" = {
+          action = "builtin";
+          options.desc = "[S]earch [S]elect Telescope";
+        };
+        "<leader>sw" = {
+          action = "grep_string";
+          options.desc = "[S]earch Current [W]ord";
+        };
+        "<leader>sg" = {
+          action = "live_grep";
+          options.desc = "[S]earch by [G]rep";
+        };
+        "<leader>sd" = {
+          action = "diagnostics";
+          options.desc = "[S]earch [D]iagnostics";
+        };
+        "<leader>sr" = {
+          action = "resume";
+          options.desc = "[S]earch [R]esume";
+        };
+        "<leader>s." = {
+          action = "oldfiles";
+          options.desc = "[S]earch Recent Files (. for repeat)";
+        };
+        "<leader><leader>" = {
+          action = "buffers";
+          options.desc = "[] Find existing buffers";
+        };
+      };
+      keymapsSilent = true;
 
-        keymapsSilent = true;
-
+      settings = {
         defaults = {
           file_ignore_patterns = [
             "^.git/"
