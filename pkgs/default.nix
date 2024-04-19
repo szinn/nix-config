04f6e08920b00a8239969859422a7d51fdf9d56e
Scotte Zinn <scotte@zinn.ca>
2024-04-18 20:11:40 -0400

    Bump flake.lock

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -4,5 +4,5 @@
 }: {
   tesla-auth = pkgs.callPackage ./tesla-auth.nix {};
   talosctl = pkgs.callPackage ./talosctl.nix {};
-  talhelper = pkgs.callPackage inputs.talhelper {};
+  talhelper = inputs.talhelper.packages.${pkgs.system}.default;
 }
