d1edcb930527fae075dc27561fd7770b52ddb6a1
Scotte Zinn <scotte@zinn.ca>
2024-04-20 07:48:00 -0400

    go 1.22.2 is now current

--- overlays/default.nix
+++ overlays/default.nix
@@ -2,16 +2,16 @@ inputs: let
   pickLatest = (import ./utils.nix).pickLatest;
 
   overrides = final: prev: {
-    go_1_22 =
-      pickLatest (prev.go_1_22.overrideAttrs (old: {
-        version = "1.22.2";
-        src = prev.fetchurl {
-          url = "https://go.dev/dl/go1.22.2.src.tar.gz";
-          # hash = lib.fakeHash;
-          hash = "sha256-N06oKyiexzjpaCZ8rFnH1f8YD5SSJQJUeEsgROkN9ak=";
-        };
-      }))
-      prev.go_1_22;
+    # go_1_22 =
+    #   pickLatest (prev.go_1_22.overrideAttrs (old: {
+    #     version = "1.22.2";
+    #     src = prev.fetchurl {
+    #       url = "https://go.dev/dl/go1.22.2.src.tar.gz";
+    #       # hash = lib.fakeHash;
+    #       hash = "sha256-N06oKyiexzjpaCZ8rFnH1f8YD5SSJQJUeEsgROkN9ak=";
+    #     };
+    #   }))
+    #   prev.go_1_22;
   };
 
   additions = final: _prev:
