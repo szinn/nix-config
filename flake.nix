232496361bd04caf86a73a577f9310a56747e578
Scotte Zinn <scotte@zinn.ca>
2024-05-01 06:47:02 -0400

    Getting ready for vyos migration

--- flake.nix
+++ flake.nix
@@ -115,7 +115,7 @@
           # $ git add . ; sudo nixos-rebuild --flake . switch
           ragnar = mkSystemLib.mkNixosSystem "x86_64-linux" "ragnar" overlays flake-packages;
           # $ git add . ; sudo nixos-rebuild --flake . switch
-          titan = mkSystemLib.mkNixosSystem "aarch64-linux" "titan" overlays flake-packages;
+          titan = mkSystemLib.mkNixosSystem "x86_64-linux" "titan" overlays flake-packages;
         };
 
         darwinConfigurations = {
