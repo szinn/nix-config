f197bd30565fbd0f755d9df778c2832f168ef381
Scotte Zinn <scotte@zinn.ca>
2024-05-04 21:58:04 -0400

    Go back to unstable minio

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,7 +2,6 @@
   inputs,
   pkgs ? import <nixpkgs> {},
 }: {
-  minio = pkgs.callPackage ./minio.nix {};
   tesla-auth = pkgs.callPackage ./tesla-auth.nix {};
   talosctl = pkgs.callPackage ./talosctl.nix {};
   talhelper = inputs.talhelper.packages.${pkgs.system}.default;
