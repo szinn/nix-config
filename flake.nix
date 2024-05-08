f9cf39f303c7e4d8c5280b7a6df2e9ef68b77668
Scotte Zinn <scotte@zinn.ca>
2024-05-08 14:59:13 -0400

    Fix hyprland build issue

--- flake.nix
+++ flake.nix
@@ -69,7 +69,7 @@
 
     # Hyprland
     hyprland = {
-      url = "github:hyprwm/Hyprland";
+      url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     hyprland-plugins = {
