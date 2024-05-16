35ebe13eb3ac08cb64c3e2ac2d264d1e153452c4
Scotte Zinn <scotte@zinn.ca>
2024-05-16 07:53:27 -0400

    Patch atuin for better zfs performance

--- overlays/default.nix
+++ overlays/default.nix
@@ -2,6 +2,17 @@ inputs: let
   inherit (import ./utils.nix) pickLatest;
 
   overrides = final: prev: {
+    # go_1_22 =
+    #   pickLatest (prev.go_1_22.overrideAttrs (old: {
+    #     version = "1.22.2";
+    #     src = prev.fetchurl {
+    #       url = "https://go.dev/dl/go1.22.2.src.tar.gz";
+    #       # hash = lib.fakeHash;
+    #       hash = "sha256-N06oKyiexzjpaCZ8rFnH1f8YD5SSJQJUeEsgROkN9ak=";
+    #     };
+    #   }))
+    #   prev.go_1_22;
+
     lua-language-server =
       pickLatest (prev.lua-language-server.overrideAttrs (old: {
         version = "3.9.1";
@@ -14,16 +25,10 @@ inputs: let
         };
       }))
       prev.lua-language-server;
-    # go_1_22 =
-    #   pickLatest (prev.go_1_22.overrideAttrs (old: {
-    #     version = "1.22.2";
-    #     src = prev.fetchurl {
-    #       url = "https://go.dev/dl/go1.22.2.src.tar.gz";
-    #       # hash = lib.fakeHash;
-    #       hash = "sha256-N06oKyiexzjpaCZ8rFnH1f8YD5SSJQJUeEsgROkN9ak=";
-    #     };
-    #   }))
-    #   prev.go_1_22;
+
+    atuin = prev.atuin.overrideAttrs (_old: {
+      patches = [./patches/0001-make-atuin-on-zfs-fast-again.patch];
+    });
   };
 
   additions = final: _prev:
