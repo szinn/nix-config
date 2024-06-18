ee1de8d72b5b6331331d0a26af614f36207a0b3f
Scotte Zinn <scotte@zinn.ca>
2024-06-18 05:42:36 -0400

    Remove nixvim follows

--- flake.nix
+++ flake.nix
@@ -56,9 +56,8 @@
 
     # VSCode community extensions
     nix-vscode-extensions = {
-      # url = "github:szinn/nix-vscode-extensions/fix-patchelf-reference";
       url = "github:nix-community/nix-vscode-extensions";
-      inputs.nixpkgs.follows = "nixpkgs";
+      # inputs.nixpkgs.follows = "nixpkgs";
     };
 
     # nixvim configuration
