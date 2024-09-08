7edc0fdb8f84ab4864e2fe7f744c3736213ca85d
Scotte Zinn <scotte@zinn.ca>
2024-09-08 15:57:49 -0400

    fix: Update nixavim config

--- modules/nvim/statuslines/lualine.nix
+++ modules/nvim/statuslines/lualine.nix
@@ -2,53 +2,56 @@
   plugins.lualine = {
     enable = true;
 
-    alwaysDivideMiddle = true;
-    globalstatus = true;
-    ignoreFocus = ["neo-tree"];
-    extensions = ["fzf"];
-    theme = "auto";
-    componentSeparators = {
-      left = "|";
-      right = "|";
-    };
+    settings = {
+      alwaysDivideMiddle = true;
+      extensions = ["fzf"];
+      globalstatus = true;
+      ignoreFocus = ["neo-tree"];
+      theme = "auto";
 
-    # +-------------------------------------------------+
-    # | A | B | C                             X | Y | Z |
-    # +-------------------------------------------------+
-    sections = {
-      lualine_a = ["mode"];
-      lualine_b = ["branch"];
-      lualine_c = ["filename" "diff"];
+      componentSeparators = {
+        left = "|";
+        right = "|";
+      };
 
-      lualine_x = [
-        "diagnostics"
+      # +-------------------------------------------------+
+      # | A | B | C                             X | Y | Z |
+      # +-------------------------------------------------+
+      sections = {
+        lualine_a = ["mode"];
+        lualine_b = ["branch"];
+        lualine_c = ["filename" "diff"];
 
-        # Show active language server
-        {
-          name.__raw = ''
-            function()
-                local msg = ""
-                local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
-                local clients = vim.lsp.get_active_clients()
-                if next(clients) == nil then
-                    return msg
-                end
-                for _, client in ipairs(clients) do
-                    local filetypes = client.config.filetypes
-                    if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
-                        return client.name
-                    end
-                end
-                return msg
-            end
-          '';
-          icon = "";
-          color.fg = "#ffffff";
-        }
-        "encoding"
-        "fileformat"
-        "filetype"
-      ];
+        lualine_x = [
+          "diagnostics"
+
+          # Show active language server
+          {
+            __unkeyed-1.__raw = ''
+              function()
+                  local msg = ""
+                  local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
+                  local clients = vim.lsp.get_active_clients()
+                  if next(clients) == nil then
+                      return msg
+                  end
+                  for _, client in ipairs(clients) do
+                      local filetypes = client.config.filetypes
+                      if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+                          return client.name
+                      end
+                  end
+                  return msg
+              end
+            '';
+            icon = "";
+            color.fg = "#ffffff";
+          }
+          "encoding"
+          "fileformat"
+          "filetype"
+        ];
+      };
     };
   };
 }
